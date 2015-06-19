111c07779ed6e4845d2d347dba13941c27e1f6c8
yingjinhui <yingjinhui@hotmail.com>
2015-06-19 11:23:58 +0800

    test for github

--- app/src/main/java/com/ace/jhying/helloworld/NextActivity.java
+++ app/src/main/java/com/ace/jhying/helloworld/NextActivity.java
@@ -12,6 +12,7 @@ public class NextActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+        //add some notice for test
     }
 
 
